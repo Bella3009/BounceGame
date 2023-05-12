54978b92b35fbf6ef6bd2818fd72fc471151fb26
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-05-12 23:55:56 +0200

    Update Bounce.js
    Add function to the button jump to start to move the circle.

--- Bounce.js
+++ Bounce.js
@@ -1,13 +1,42 @@
 window.onload = function() {
-   let btn = document.getElementById("jump");
-   let context = canvas.getContext("2d");
-    
-   // Axis for the shape position
-   var x = 300;
-   var y = 350;
+    let btn = document.getElementById("jump");
+    let context = canvas.getContext("2d");
 
-   // Initialise the shape
-   context.arc(x, y, 50, 0, 2 * Math.PI);
-   context.fillStyle="red";
-   context.fill();
+    // Axis for the shape position
+    var x = 300;
+    var y = 350;
+
+    // Initialise the shape
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle="red";
+    context.fill();
+
+    btn.onclick = function() {
+       count += 1;
+    }
+
+    btn.onclick = function() {
+       count += 1;
+       // Changing the y position
+       y -= 25;
+
+       // Shape return to original position when hit the corner
+       if(y<=-50){
+           y = 350;
+       }
+
+       // Clearing the canvas
+       context.clearRect(0, 0, 600, 400);
+
+       // Redrawing the circle
+       context.beginPath();
+       context.arc(x, y, 50, 0, 2 * Math.PI);
+       context.fillStyle="red";
+       context.fill();
+
+       //Drawing the count value
+       context.font = '25px Arial';
+       context.fillStyle = 'white';
+       context.fillText("Count: " + count, 20, 30);
+    }
 }
