47e39ea0099f5e8f3e9c3fac1dd24b55e0ceadd9
Bella3009 <bellasara13@gmail.com>
2023-05-14 21:23:24 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Bounce.js

--- Bounce.js
+++ Bounce.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let btn = document.getElementById("jump");
-    let context = canvas.getContext("2d");
+    let context = canvas.getContext("2d")
     let count = 0;
 
     // Axis for the shape position
@@ -13,34 +13,30 @@ window.onload = function() {
     context.fill();
 
     btn.onclick = function() {
-       count += 1;
-    }
-
-    btn.onclick = function() {
         count += 1;
         // Changing the y position
         y -= 25;
         draw();
 
-        function draw(){
-            // Shape return to original position when hits the corner
-            if(y<=-50){
-                y = 350;
-            }
+    function draw(){
+        // If the circle touch the top returns to original position
+        if(y<=25){
+            y = 350;
+        }
+        //Clearing the canvas
+        context.clearRect(0, 0, 600, 400);
 
-            // Clearing the canvas
-            context.clearRect(0, 0, 600, 400);
+        //Redrawing the circle
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2 * Math.PI);
+        context.fillStyle="red";
+        context.fill();
 
-            // Redrawing the circle
-            context.beginPath();
-            context.arc(x, y, 50, 0, 2 * Math.PI);
-            context.fillStyle="red";
-            context.fill();
-
-            //Drawing the count value
-            context.font = '25px Arial';
-            context.fillStyle = 'white';
-            context.fillText("Count: " + count, 20, 30);
-            window.requestAnimationFrame(draw);
+        //Drawing the count value
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText("Count: " + count, 20, 30);
+        window.requestAnimationFrame(draw);
+        }
     }
 }
